d66f1a5fc36f90302b09cb45f686cd9f15f7b6cd
WaterLemur <38868316+WaterLemur@users.noreply.github.com>
2026-09-16 14:06:36 -0500

    ***

---
+++ src/LearningUnreal/Source/LearningUnreal/Private/MyTestActor.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyTestActor.h"
+
+// Sets default values
+AMyTestActor::AMyTestActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMyTestActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+
+	Test();
+	
+}
+
+// Called every frame
+void AMyTestActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AMyTestActor::Test()
+{
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, TEXT("Hello from Bindows!"));
+	}
+}
+
---
+++ src/LearningUnreal/Source/LearningUnreal/Public/MyTestClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class LEARNINGUNREAL_API MyTestClass
+{
+public:
+	MyTestClass();
+	~MyTestClass();
+};
